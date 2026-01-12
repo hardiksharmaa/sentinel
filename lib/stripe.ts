fe98103b811239617262cc04c91852c4e27194cd
Hardik <hs489819@gmail.com>
2026-01-13 00:03:35 +0530

    stripe fix

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: "2023-10-16", // Use the latest version available
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-12-15.clover", // <--- MATCHES THE ERROR MESSAGE
   typescript: true,
 });
